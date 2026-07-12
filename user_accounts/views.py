50fa55fd4d5c7647e3b195084de30fef85f9f73d
Md. Masudur Rahman <masudur.rahman0501@gmail.com>
2026-07-13 02:56:00 +0600

    added homepage logo and rgister option

--- user_accounts/views.py
+++ user_accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def login_view(request):
@@ -29,3 +30,56 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("shop:home")
+
+
+
+
+def register_view(request):
+
+    if request.method == "POST":
+
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+
+        if password != confirm_password:
+
+            return render(request, "register.html", {
+                "error": "Password does not match!"
+            })
+
+
+        if User.objects.filter(username=username).exists():
+
+            return render(request, "register.html", {
+                "error": "Username already exists!"
+            })
+
+
+        if User.objects.filter(email=email).exists():
+
+            return render(request, "register.html", {
+                "error": "Email already exists!"
+            })
+
+
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            password=password
+        )
+
+
+        user.save()
+
+
+        return redirect("user_accounts:login")
+
+
+    return render(request, "register.html")
